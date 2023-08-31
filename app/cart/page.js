ffaa549006c88487d808d105a22467923c60c00b
coaudtn0276 <coaudtn0276@gmail.com>
2023-08-31 19:35:22 +0900

    페이지 라우팅

---
+++ app/cart/page.js
@@ -0,0 +1,7 @@
+export default function Cart() {
+  return (
+    <div>
+      <h4 className="title">장바구니 입니다.</h4>
+    </div>
+  );
+}
---
+++ app/list/def/page.js
@@ -0,0 +1,7 @@
+export default function List() {
+  return (
+    <div>
+      <h4>def페이지임</h4>
+    </div>
+  );
+}
